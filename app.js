2744c6b85c8119b4100578e597a78513422d3109
Alex Bond <a.bond@nhm.ac.uk>
2026-09-15 11:04:08 +0100

    Add correction form link

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 // ---------------------------------------------------------------------
 // CONFIG
 // ---------------------------------------------------------------------
-// Replace with your actual "submit a correction" form once built (Google
-// Forms / MS Forms). If the form supports pre-filled fields, add the
-// institution name as a query param matching your form's field ID -
-// see https://support.google.com/docs/answer/160000 ("Get pre-filled link").
-const CORRECTION_FORM_BASE_URL = "https://forms.gle/REPLACE_ME";
+const CORRECTION_FORM_BASE_URL = "https://forms.cloud.microsoft/Pages/ResponsePage.aspx?id=AZyic3hOf0Og1MhVPhlgwaRoQWIuOpdOmEogSlS-oFhUQjVUMktROE9XNjVaS1hEUzI4NlNHQkJKNS4u";
 
 // ---------------------------------------------------------------------
 
